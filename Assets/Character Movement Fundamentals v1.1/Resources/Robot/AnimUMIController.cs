8ce48d9c1423b0db605e3ea5bd8332e3e23d25f6
Arthur4329 <60528031+Arthur4329@users.noreply.github.com>
2020-07-17 11:14:23 +0200

    Initial commit

---
+++ Assets/Character Movement Fundamentals v1.1/Resources/Robot/AnimUMIController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimUMIController : MonoBehaviour
+
+{
+    public Animator anim;
+    public GameObject UMIModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            anim.SetBool("OnWalk", true);
+            UMIModel.GetComponent<Animator>().Play("Armature_001_Walking");
+        }
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetBool("OnJump", true);
+            UMIModel.GetComponent<Animator>().Play("Armature_001_Jump");
+
+        }
+        if (Input.GetButtonDown("Looking"))
+        {
+            anim.SetBool("IsLooking", true);
+            UMIModel.GetComponent<Animator>().Play("Armature_001_IdleLooking");
+
+        }
+
+        else
+                {
+            anim.SetBool("isWalking", false);
+        }
+    }
+}
---
+++ Assets/Character Movement Fundamentals v1.1/Resources/Robot/Deplacement.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+
+public class Deplacement : MonoBehaviour
+{
+
+    //Slope limit;
+    [Range(0f, 90f)]
+    [SerializeField] public float slopeLimit = 30f;
+
+    //Collider variables;
+    [Range(0f, 1f)] public float stepHeight = 0.25f;
+    [SerializeField] public float colliderHeight = 2f;
+    [SerializeField] public float colliderThickness = 1f;
+    [SerializeField] public Vector3 colliderOffset = Vector3.zero;
+
+    //References to attached collider(s);
+    BoxCollider boxCollider;
+    SphereCollider sphereCollider;
+    CapsuleCollider capsuleCollider;
+
+
+    BasicWalkerController controller;
+    public int speed = 5;
+    public int coefRot = 200;
+    private Animator anim;
+    
+
+
+    void Awake()
+    {
+        anim = GetComponentInChildren<Animator>();
+        
+    }
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+
+    {
+
+
+        //Déplacement
+        transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * coefRot * Time.deltaTime);
+
+
+        //Animations
+
+        ///MARCHE
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            anim.SetBool("Walk", true);
+
+
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+
+        }
+        
+
+        /// MARCHE + JUMP
+        if ((Input.GetAxis("Vertical") > 0)) 
+            if (Input.GetKeyDown(KeyCode.C))
+
+        {
+            
+            anim.SetBool("Walk", false);
+            anim.SetTrigger("JumpForward");
+
+
+        }
+        else
+        {
+            anim.SetBool("Walk", true);
+
+        }
+
+
+
+        /// JUMP SUR PLACE
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("Jump");
+        }
+
+        
+        
+        /// LOOKING
+        if (Input.GetButtonDown("LookingTo"))
+        {
+            anim.SetTrigger("LookingTo");
+        }
+
+
+        ///MARCHE ARRIERE
+        //if (VerticalSpeed < 0)
+        //{
+        // anim.Trigger("Fall");
+
+        //}
+
+
+
+
+
+    }
+}
